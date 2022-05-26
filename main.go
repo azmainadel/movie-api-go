13aa3b1e551165a0d19541ad5ccee046dbbcd3f7
azmainadel <azmainadel47@gmail.com>
2022-05-26 16:58:48 +0600

    :zap: Add runner file and set API endpoints

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/azmainadel/movie-api-go/handler"
+	"github.com/azmainadel/movie-api-go/repository"
+	"github.com/azmainadel/movie-api-go/service"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	movieLocalRepository := repository.NewLocalMovieRepository()
+	movieService := service.NewDefaultMovieService(movieLocalRepository)
+	movieHandler := handler.NewMovieHandler(movieService)
+
+	router := httprouter.New()
+
+	router.GET("/movies", movieHandler.GetAllMovies)
+	router.GET("/movies/:id", movieHandler.GetMovie)
+
+	router.POST("/create-movie", movieHandler.CreateMovie)
+
+	router.PATCH("/update-movie", movieHandler.UpdateMovie)
+
+	router.DELETE("/delete-movies", movieHandler.DeleteAllMovies)
+	router.PATCH("/delete-movies/:id", movieHandler.DeleteMovie)
+
+	log.Println("HTTP server runs on :8080")
+	err := http.ListenAndServe(":8080", router)
+
+	log.Fatal(err)
 }
